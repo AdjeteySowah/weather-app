54af54ec347f8b07deadd3ab22f6a091c3e87b6a
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-05-20 22:28:20 +0000

    Configure ESLint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+   extends: ["airbnb-base", "prettier"],
+   // Optionally add your project-specific rules or settings here:
+   rules: {
+     // Example: Allow console statements:
+     "no-console": "off",
+   },
+};
