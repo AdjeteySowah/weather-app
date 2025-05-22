9b06de57121a75f1336ef290ca9fd95bf64f885c
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-05-22 23:20:09 +0000

    Remove the configuration/bundling for images

--- webpack.common.js
+++ webpack.common.js
@@ -1,31 +1,24 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-   entry: "./src/index.js",
+   entry: './src/index.js',
    cache: {
-      type: "filesystem",
+      type: 'filesystem',
       buildDependencies: {
-        config: [__filename],
+         config: [__filename],
       },
    },
    module: {
       rules: [
          {
             test: /\.html$/i,
-            loader: "html-loader",
+            loader: 'html-loader',
          },
          {
             test: /\.(png|svg|jpg)$/i,
-            type: "asset/resource",
-            generator: {
-               filename: "assets/images/[name].[contenthash][ext]",
-            },
-         },
-         {
-            test: /\.(woff|woff2)$/i,
             type: 'asset/resource',
             generator: {
-               filename: "assets/fonts/[name].[contenthash][ext]",
+               filename: 'assets/images/[name].[contenthash][ext]',
             },
          },
       ],
